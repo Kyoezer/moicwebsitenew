a8be30cdaaec87c598e8231c3199bf8a3c1c3a17
krishna4810 <krishna.sharma@selise.ch>
2022-04-09 16:12:22 +0600

    events

--- indexapp/urls.py
+++ indexapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('',views.home, name='home'),#url mapping with view 
+    path('',views.home, name='home'),#url mapping with view
+    path('vacancy/<int:id>',views.vacancy_detail, name='vacancy_detail'),#url mapping with view
+    path('<int:id>',views.detail, name='detail'),#url mapping with view
 ]
--- indexapp/views.py
+++ indexapp/views.py
@@ -2,15 +2,25 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from indexapp.models import post
+from indexapp.models import post, event, vacancie
 from django.core.mail import send_mail, BadHeaderError
 
 # Create your views here.
 def home(request):
     popuplarPs = post.objects.all()
-    return render(request, 'home.html', {'post': popuplarPs, })
+    events = event.objects.all().order_by('-id')
+    vacancies = vacancie.objects.all().order_by('-id')
+    return render(request, 'home.html', {'post': popuplarPs, 'events': events, 'vacancies': vacancies})
+
+def vacancy_detail (request, id):
+    obj =get_object_or_404(vacancie, pk=id)
+    return render(request, 'vacancy_detail.html', {'obj': obj })
+
+def detail (request, id):
+    obj =get_object_or_404(event, pk=id)
+    return render(request, 'detail.html', {'obj': obj })
 
 
     
