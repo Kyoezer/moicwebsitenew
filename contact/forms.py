c5fe2fcb10f8fc32434d43502c4a77d4489069c2
krishna4810 <krishnaprasadsharma84@gmail.com>
2022-05-16 12:15:07 +0600

    fix form css

--- contact/forms.py
+++ contact/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, Textarea
+from django.forms import ModelForm, Textarea, TextInput, EmailInput
 from .models import ContactForm
 
 
@@ -7,7 +7,22 @@ from .models import ContactForm
 class ContactForm(ModelForm):
     class Meta:
         model = ContactForm
+        fields = ['first_name', 'last_name', 'email_address', 'message']
         widgets = {
-            'message': Textarea(attrs={'cols': 30, 'rows': 10}),
+            'message': Textarea(attrs={'cols': 30, 'rows': 5,
+                                        'class': "inputcontent"}),
+            'first_name': TextInput(attrs={
+                            'class': "inputcontent",
+                            'placeholder': 'First Name'
+                            }),
+            'last_name': TextInput(attrs={
+                            'class': "inputcontent",
+                            'placeholder': 'Last Name'
+                            }),
+            'email_address': EmailInput(attrs={
+                            'class': "inputcontent",
+                            'placeholder': 'Email'
+                            })
         }
-        fields = '__all__'
+
+
