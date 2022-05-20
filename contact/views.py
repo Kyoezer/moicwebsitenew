c7df3630618a2646bf1d086abd38ad7c4f2d5e3e
Kinley Yoezer <kyoezer@moic.gov.bt>
2022-05-20 20:07:58 +0600

    some minor changes

--- contact/views.py
+++ contact/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -14,20 +16,21 @@ def contact(request):
         contactForm = ContactForm(request.POST)
         if contactForm.is_valid():
             contactForm.save()
-#             subject = "Website Inquiry"
-#             body = {
-#             'first_name': contactForm.cleaned_data['first_name'],
-#             'last_name': contactForm.cleaned_data['last_name'],
-#             'email': contactForm.cleaned_data['email_address'],
-#             'message':contactForm.cleaned_data['message'],
-#             }
-#             message = "\n".join(body.values())
+            subject = "Website Inquiry"
+            body = {
+            'first_name': contactForm.cleaned_data['first_name'],
+            'last_name': contactForm.cleaned_data['last_name'],
+            'email': contactForm.cleaned_data['email_address'],
+            'message':contactForm.cleaned_data['message'],
+            }
+            message = "\n".join(body.values())
+            messages.success(request, 'You have successfully contacted to MoIC')
+
+            try:
+                send_mail(subject, message, 'email', ['kyoezer@moic.gov.bt'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect("/contact")
+    return render(request, "contact.html", {'form': form})
 #
-#             try:
-#                 send_mail(subject, message, 'kyoezer@moic.gov.bt', ['kyoezer@moic.gov.bt'])
-#             except BadHeaderError:
-#                 return HttpResponse('Invalid header found.')
-#             return redirect ("/contact")
-    return render(request, "contact.html", {'form':form})
-#
-# 	form = ContactForm()
+	# form = ContactForm()
